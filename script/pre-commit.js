c70c3859c04b94403e3732e7a99593d90d5dd448
pengzhen <529629354@qq.com>
2017-09-19 12:27:05 +0800

    123123132222

--- script/pre-commit.js
+++ script/pre-commit.js
@@ -1,6 +1,17 @@
+require('./color');
+const logError = msg => { throw new Error(msg.red) };
+const logr = msg => console.log(msg.green);
 const execSync = require('child_process').execSync;
 
 execSync('git fetch -p');
 
-console.log(12312312);
-thorw new Error('HAHAHAHA');
+// logError('HAHAHAHA');
+
+exports.validateMessage = function validateMessage(raw) {
+  var messageWithBody = (raw || '').split('\n').filter(function (str) {
+    return str.indexOf('#') !== 0;
+  }).join('\n');
+
+  var message = messageWithBody.split('\n').shift();
+  console.log(message);
+};
